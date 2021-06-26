47b9427e6e6caddad1eae2ab4d5be93545b375e8
Samm014 <samuel.medeiros014@gmail.com>
2021-06-26 19:55:32 -0300

    update calculadora

--- js/math.js
+++ js/math.js
@@ -1,29 +1,40 @@
 $(()=>{
-
-    resul = $(".resul-calculadora");
+    var novo_numero = true;
+    var total;
+    var resul = $(".resul-calculadora");
     calc = (e)=>{
-        operador = e.getAttribute('value')
-        var number = e.getAttribute('name');
-        var format_number = '<p style="font-size: 30px;">'+ number +'</p>'
-        resul.html(format_number)
+        
+        if(novo_numero){
+            total = e.getAttribute('name');
+            novo_numero = false;
+        }else{
+            total += e.getAttribute('name');
+        }
 
+        if(e.getAttribute('name') == 'clear'){
+            total = 0;
+            resul.html('<h3>'+ '' +'</h3>')
+        }
+
+        return total;
     }
     
-
     
-    
-    
-    $('.btn-igual').click(()=>{
-        
-        
-
-
-
-
-
-        resul.html('<p>'+ calc() +'</p>')
+    $('.btn').click(()=>{
+        resul.html('<h3>'+ total +'</h3>')
+        if(total == 0){
+            total = '';
+        }
     })
 
 
+   
+
+    $('.btn-igual').click(()=>{
+        var resultado = $(".resul-calculadora h3").html();
+        $('.resul-calculadora h3').html(eval(resultado))
+    })
+        
+        
 
 })
